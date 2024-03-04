7cc6bff12fbc6ee8e35a2451f45808ffa53d1952
DanMarBar <martinez.badan22@zaragoza.salesianos.edu>
2024-03-04 18:35:34 +0100

    arreglos

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {getCities} from "../controller/City.js"
-
-const router = express.Router();
-
-router.post("/getCities", getCities)
-
-
-export default router
